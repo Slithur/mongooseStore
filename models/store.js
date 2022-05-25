7e8105177c695f810a28b32938a48afcd68475e5
mahamedaden <joali218@gmail.com>
2022-05-25 16:34:26 -0500

    hw is ready

--- models/store.js
+++ models/store.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const storeSchema = new mongoose.Schema({
-	Name: { type: String, required: true },
-    description: { type: String, required: true },
-    img: { type: String, required: true },
-	completed: Boolean,
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  img: { type: String },
+  price: [Number],
+  qty: [Number]
+  //   completed: Boolean
 });
 
 const Store = mongoose.model('Store', storeSchema);
